d69181a48eb44033a519975bf1e635fe07743768
BuildTools <jaxmontelongo@gmail.com>
2020-11-11 14:14:58 -0600

    Setting Up

---
+++ src/dev.jaxi/WebScraper.java
@@ -0,0 +1,16 @@
+package dev.jaxi;
+
+public class WebScraper {
+
+    public static void main(String[] args) {
+        System.out.println("testing");
+    }
+
+
+}
+
+/*
+URL url = new URL( "http://download.me/" );
+Files.copy( url.openStream(), Paths.get("downloaded.html" ) );
+from: https://stackoverflow.com/questions/238547/how-do-you-programmatically-download-a-webpage-in-java
+ */
