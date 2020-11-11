417196fb38c9e11c1cc76bebb9128e2234c41e38
TheBomber <ethanallen0246@gmail.com>
2020-11-11 15:53:36 -0600

    new stuff

--- src/dev.jaxi/DataHouse.java
+++ src/dev.jaxi/DataHouse.java
@@ -1,5 +1,47 @@
 package dev.jaxi;
-
+import java.util.*;
 public class DataHouse {
-//google
+    private ArrayList<String> uniqueWords;
+    private TreeMap<String,Integer> unWordCount;
+    private TreeMap<TagType,Integer> tagScores;
+    public ArrayList<String> getUniqueWords() {
+        return uniqueWords;
+    }
+    public TreeMap<String, Integer> getUnWordCount() {
+        return unWordCount;
+    }
+    public TreeMap<TagType, Integer> getTagScores() {
+        return tagScores;
+    }
+    public void setTagScores(TreeMap<TagType, Integer> tagScores) {
+        this.tagScores = tagScores;
+    }
+    public void setUniqueWords(ArrayList<String> uniqueWords) {
+        this.uniqueWords = uniqueWords;
+    }
+    public void setUnWordCount(TreeMap<String, Integer> unWordCount) {
+        this.unWordCount = unWordCount;
+    }
+    public static void main(String[] args){
+        String[] f="f f f f ff f f f f f f f f f f, f f f".split("[ ,]+");
+    System.out.println(f);
+    }
+}
+class TagType {
+    public int value;
+    public TagType(String x){
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+    public int compareTo(TagType o) {
+        return value-o.getValue();
+    }
+
+
+
 }
