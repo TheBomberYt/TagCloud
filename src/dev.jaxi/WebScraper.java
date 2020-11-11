45e4ed23e824a8ab7f98f4d719661ba7430bd83e
BuildTools <jaxmontelongo@gmail.com>
2020-11-11 14:33:07 -0600

    Download webpage

--- src/dev.jaxi/WebScraper.java
+++ src/dev.jaxi/WebScraper.java
@@ -1,9 +1,20 @@
 package dev.jaxi;
 
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class WebScraper {
 
     public static void main(String[] args) {
+
         System.out.println("testing");
+        try {
+            URL url = new URL("https://jaxi.dev");
+            Files.copy(url.openStream(), Paths.get("index.html"));
+        } catch(Exception e) {
+            System.out.print(e);
+        }
     }
 
 
