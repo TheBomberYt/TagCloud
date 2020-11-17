8c27ab0ab5078fa4e8f9ef10af88ed7d6bd5a6e1
BuildTools <jaxmontelongo@gmail.com>
2020-11-17 17:47:19 -0600

    Removed un-needed imports

--- src/dev.jaxi/WebScraper.java
+++ src/dev.jaxi/WebScraper.java
@@ -1,16 +1,11 @@
 package dev.jaxi;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+
 
 
 public class WebScraper {
