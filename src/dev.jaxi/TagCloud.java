76217a48dd444d49a84a62a33d88162f2ea95175
BuildTools <jaxmontelongo@gmail.com>
2020-11-17 12:40:17 -0600

    tagcloud v2

--- src/dev.jaxi/TagCloud.java
+++ src/dev.jaxi/TagCloud.java
@@ -4,31 +4,31 @@ import java.util.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class TagCloud() {
+public class TagCloud {
     public TagCloud(TreeMap<String, Integer> test) {
 
 
         TagCloudCanvas canvas = new TagCloudCanvas();
         TagCloudJFrame jframe = new TagCloudJFrame();
     }
-}
 
-
-public class TagCloudCanvas extends Canvas {
-   private
-    public TagCloud(){
+    public TagCloud() {
 
     }
+}
+
+
+class TagCloudCanvas extends Canvas {
 
 }
 
-public class TagCloudJFrame extends JFrame{
+class TagCloudJFrame extends JFrame{
     public static final int WIDTH=1920;
     public static final int HEIGHT=1080;
 
     public void TagCloudRunner(){
         setSize(WIDTH,HEIGHT);
-        getContentPane().add( new TagCloud() );
+        getContentPane().add(new TagCloudJFrame());
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
